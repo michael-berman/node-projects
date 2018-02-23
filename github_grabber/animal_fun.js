bfc7d592ae5b44a8f05343b1fb8c0005a4e79b9e
Michael Berman <michael.v.berman@gmail.com>
2018-02-23 10:26:47 -0500

    Finish adding server to animal reader

--- github_grabber/animal_fun.js
+++ github_grabber/animal_fun.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
-const querystring = require('querystring');
+const qs = require('querystring');
+const cache = {};
 
 // fs.readFile('./animals.txt', 'utf-8', (err, data) => {
 //   if (err) {
@@ -23,9 +24,46 @@ const querystring = require('querystring');
 //     console.log(`${process.argv[2]}_animals successfully written!`);
 //   })
 // })
+
 const server = http.createServer((req, res) => {
-  res.write('hello world');
-  res.end();
+  const query = req.url.split("?")[1];
+
+  if (query !== undefined) {
+
+    const parsedLetter = Object.keys(qs.parse(query))[0].toUpperCase();
+
+    if (cache[parsedLetter] !== undefined) {
+      res.end(`${cache[parsedLetter]}`);
+    }
+
+    if (parsedLetter !== undefined) {
+
+      fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+
+        if (err) {
+          console.log(err);
+          res.end('It failed');
+          return
+        }
+
+        let animals = data.split("\n");
+        let animalsSelected = [];
+        animals.forEach( animal => {
+          if (animal[0] === parsedLetter) {
+            animalsSelected.push(animal);
+          }
+        })
+
+        cache[parsedLetter] = animalsSelected;
+
+
+      })
+    }
+
+    res.end(`${cache[parsedLetter]}`);
+
+  }
+
 })
 
 server.listen(8000, () => console.log("I'm listening on port 8000!"));
