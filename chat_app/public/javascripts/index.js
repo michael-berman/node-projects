3034b408104ba8959e995fe86ba0577be0c66799
Michael Berman <michael.v.berman@gmail.com>
2018-02-26 13:30:42 -0500

    add server and configure webpack

---
+++ chat_app/public/javascripts/index.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+  const io = require('socket.io-client');
+  const socket = io();
+  console.log(socket);
+  console.log('hello');
+// });
