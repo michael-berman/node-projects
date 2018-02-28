27b95554ceb5ffe0a1eada74628a175418e63349
Michael Berman <michael.v.berman@gmail.com>
2018-02-28 12:56:26 -0500

    add a process command to name change

--- chat_app/public/javascripts/chat.js
+++ chat_app/public/javascripts/chat.js
@@ -6,6 +6,22 @@ class Chat {
   sendMessage (msg) {
     this.socket.emit('message', {text: msg});
   }
+
+  processCommand (command) {
+    let message = false;
+
+    switch (command) {
+      case 'nick':
+        this.socket.emit('nameAttempt', command);
+        break;
+      default:
+        message = 'Unrecognized command';
+        break;
+    }
+
+    return message;
+  }
+
 }
 
 module.exports = Chat;
