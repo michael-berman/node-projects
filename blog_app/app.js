87b253cefd626e7736debeee6817e4b87e2f57bf
Michael Berman <michael.v.berman@gmail.com>
2018-03-02 12:18:39 -0500

    add env variables

--- blog_app/app.js
+++ blog_app/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+require('dotenv').config({ path: './variables.env' });
 
 app.get("/", (req, res) => res.end('this is the response'));
 
 app.use(morgan('combined'));
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
