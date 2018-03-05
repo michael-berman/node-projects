2964e8b926ba48827701aad667b237536a845026
Michael Berman <michael.v.berman@gmail.com>
2018-03-05 13:15:33 -0500

    display blogs in index view

--- blog_app/app.js
+++ blog_app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config({ path: './variables.env' });
 
 app.set('views', 'views');
@@ -9,12 +10,20 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'styles')));
 
-app.get("/", (req, res) => {
-  res.render('index', {blogs: []});
-});
 
 app.use(morgan('combined'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
+});
+
+// Temporary blog array
+
+const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8');
+const blogArray = JSON.parse(blogFile);
+
+// Routes
+app.get("/", (req, res) => {
+  console.log(blogArray);
+  res.render('index', {blogs: blogArray});
 });
