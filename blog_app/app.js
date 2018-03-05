5439b7d5c2289902e8eb6950cf236271f70a2d1d
Michael Berman <michael.v.berman@gmail.com>
2018-03-05 12:32:46 -0500

    add mixins, layout, index views and styles ref

--- blog_app/app.js
+++ blog_app/app.js
@@ -7,8 +7,10 @@ require('dotenv').config({ path: './variables.env' });
 app.set('views', 'views');
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'styles')));
+
 app.get("/", (req, res) => {
-  res.render('hello.pug', {header: "hello all"});
+  res.render('index', {blogs: []});
 });
 
 app.use(morgan('combined'));
